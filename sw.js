6988fecc7dec07daa6309522538ef70e530bc059
juansantana-analista <juansantana.analista@gmail.com>
2024-09-10 10:48:29 -0300

    Manifest.json change

--- sw.js
+++ sw.js
@@ -81,8 +81,5 @@ importScripts("https://cdn.pushalert.co/sw-74144.js");
     });
 
     self.addEventListener('notificationclick', (event) => {
-        event.notification.close(); 
-        var fullPath = self.location.origin + event.notification.data.path; 
-        clients.openWindow(fullPath); 
-        alert('notificaçõ clicada');
+        app.dialog.alert('notificaçõ clicada');
     });
