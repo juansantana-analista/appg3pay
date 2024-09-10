c8c37b8adf82d6a164bf0a82a7b12a999fc61acd
juansantana-analista <juansantana.analista@gmail.com>
2024-09-10 09:29:14 -0300

    Manifest.json change

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-importScripts("https://cdn.pushalert.co/sw-74144.js");
     const HOSTNAME_WHITELIST = [
         self.location.hostname,
         'fonts.gstatic.com',
