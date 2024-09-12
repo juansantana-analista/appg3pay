a696b0e9084f64eb9c49b614705048edd1df5a3a
juansantana-analista <juansantana.analista@gmail.com>
2024-09-12 12:01:00 -0300

    Notificaçao android instalado

--- js/plataforma.js
+++ js/plataforma.js
@@ -4,3 +4,8 @@ if (window.matchMedia('(display-mode: fullscreen)').matches) {
     alert('O app não está instalado.');
   }
   
+  if (window.navigator.standalone) {
+    alert('O app está instalado e rodando em modo standalone');
+  } else {
+    alert('O app não está instalado.');
+  }
