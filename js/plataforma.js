4be0a6fdfff566f478bd007a7aaa30e3d1ef4f6e
juansantana-analista <juansantana.analista@gmail.com>
2024-09-12 11:58:54 -0300

    Notificaçao android instalado

--- js/plataforma.js
+++ js/plataforma.js
@@ -1,4 +1,4 @@
-if (window.matchMedia('(display-mode: standalone)').matches) {
+if (window.matchMedia('(display-mode: fullscreen)').matches) {
     alert('O app está instalado e rodando em modo standalone');
   } else {
     alert('O app não está instalado.');
