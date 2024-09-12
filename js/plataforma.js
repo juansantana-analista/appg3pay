119b444bb7473c9bf479b72babfa005321abf70b
juansantana-analista <juansantana.analista@gmail.com>
2024-09-12 13:24:21 -0300

    Notificaçao android instalado

--- js/plataforma.js
+++ js/plataforma.js
@@ -44,33 +44,24 @@ if (window.matchMedia('(display-mode: standalone)').matches || window.matchMedia
   
   // Use this function to take actions based on the platform
   const platform = detectPlatform();
+  var conteudoInstall = document.getElementById('conteudoInstall');
   
   if (platform === 'iOS') {
     // Ações específicas para iOS
-    
+    conteudoInstall.innerHTML = `
+        <p>Adicione <strong>o aplicativo G3 Pay</strong> à sua tela inicial para obter atualizações regulares. Toque em Compartilhar 
+        <span class="mdi mdi-export-variant"></span> e depois <strong>Adicionar à <br>tela inicial </strong><span class="mdi mdi-plus-box-outline"></span>
+        </p>`;    
   } else if (platform === 'Android') {
     // Ações específicas para Android
+            conteudoInstall.innerHTML = `
+            <p>Instale <strong>o aplicativo G3 Pay</strong> para obter atualizações regulares. É rápido e ocupa menos armazenamento</p>
+            <div class="display-flex flex-direction-row justify-content-space-between">
+            <button class="button margin-right text-color-black">Depois</button>
+            <button class="button button-fill color-red"><span class="mdi mdi-cellphone-arrow-down-variant"></span> Instalar</button>
+            </div>`;
     
   } else {
     // Ações para desktop ou plataformas desconhecidas
     
   }
-  
-  
-  document.addEventListener('DOMContentLoaded', function() {
-        var conteudoInstall = document.getElementById('conteudoInstall');
-
-        if (conteudoInstall && platform === 'iOS') {
-            conteudoInstall.innerHTML = `
-                <p>Adicione <strong>o aplicativo G3 Pay</strong> à sua tela inicial para obter atualizações regulares. Toque em Compartilhar 
-                <span class="mdi mdi-export-variant"></span> e depois <strong>Adicionar à <br>tela inicial </strong><span class="mdi mdi-plus-box-outline"></span>
-                </p>`;
-        } else {
-            conteudoInstall.innerHTML = `
-                <p>Instale <strong>o aplicativo G3 Pay</strong> para obter atualizações regulares. É rápido e ocupa menos armazenamento</p>
-                <div class="display-flex flex-direction-row justify-content-space-between">
-                <button class="button margin-right text-color-black">Depois</button>
-                <button class="button button-fill color-red"><span class="mdi mdi-cellphone-arrow-down-variant"></span> Instalar</button>
-                </div>`;
-        }
-   });
