b3714992488d92efc152804f71f598032d22cda8
juansantana-analista <juansantana.analista@gmail.com>
2024-09-12 11:29:16 -0300

    Notificaçao android instalado

---
+++ js/plataforma.js
@@ -0,0 +1,15 @@
+function pedirPermissaoNotificacoes() {
+    if ('Notification' in window) {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('Permissão de notificações concedida');
+          // Aqui você pode mostrar uma notificação de teste ou iniciar o serviço
+          mostrarNotificacaoTeste();
+        } else {
+          console.log('Permissão de notificações negada');
+        }
+      });
+    }
+  }
+
+  pedirPermissaoNotificacoes();
