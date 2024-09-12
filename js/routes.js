04b435e3cf5274aad3ea8dde1e5e9b0766e529ef
juansantana-analista <juansantana.analista@gmail.com>
2024-09-12 15:24:09 -0300

    Notificaçao android instalado

--- js/routes.js
+++ js/routes.js
@@ -506,12 +506,7 @@ var app = new Framework7({
         },
         pageInit: function (event, page) {          
           OneSignal.Notifications.requestPermission();
-          const options = {method: 'GET', headers: {accept: 'application/json'}};
 
-          fetch('https://api.onesignal.com/apps/app_id/subscriptions/subscription_id/user/identity', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
           // fazer algo quando a página for inicializada  
           $.getScript('js/qrcode.min.js');
           onDashboard();
