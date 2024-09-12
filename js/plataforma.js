d9c22cce1b7c08f33ec14319687fa7d101b04975
juansantana-analista <juansantana.analista@gmail.com>
2024-09-12 11:13:12 -0300

    Notificaçao android instalado

--- js/plataforma.js
+++ js/plataforma.js
@@ -3,47 +3,46 @@ function detectMobileOS() {
   
     // Verifica se é um dispositivo iOS (iPhone/iPad)
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-      return 'iOS';
+        return 'iOS';
     }
   
     // Verifica se é um dispositivo Android
     if (/android/i.test(userAgent)) {
-      return 'Android';
+        return 'Android';
     }
   
     // Caso não seja iOS nem Android
     return 'Other';
-  }
+}
   
-  const os = detectMobileOS();
-  console.log('Sistema Operacional:', os);
-  
-  // Você pode usar essa variável `os` para tomar ações específicas
-  if (os === 'iOS') {
+const os = detectMobileOS();
+console.log('Sistema Operacional:', os);
+
+// Verificar se o PWA está instalado com base no sistema operacional
+if (os === 'iOS') {
     var estaInstalado = onApplePwa();
-    alert(estaInstalado);
-  } else if (os === 'Android') {
+    alert("PWA instalado no iOS: " + estaInstalado);
+} else if (os === 'Android') {
     var estaInstalado = onAndroidPwa();
-    alert(estaInstalado);
-  } else {
+    alert("PWA instalado no Android: " + estaInstalado);
+} else {
     console.log('Usuário está usando outro tipo de dispositivo.');
-  }
-  
+}
 
-  function onAndroidPwa () {
-    let isPWAInstalled = false;
-    // Verifica se o PWA está instalado
+function onAndroidPwa() {
+    // Verifica se o PWA está instalado no Android
     if (window.matchMedia('(display-mode: standalone)').matches) {
-        isPWAInstalled = true;
-        return isPWAInstalled;
+        return true;
+    } else {
+        return false;
     }
-  }
+}
 
-  function onApplePwa () {
-    let isPWAInstalled = false;
-    // Verifica se o PWA está instalado
+function onApplePwa() {
+    // Verifica se o PWA está instalado no iOS
     if (window.navigator.standalone) {
-        isPWAInstalled = true;
-        return isPWAInstalled;
+        return true;
+    } else {
+        return false;
     }
-  }
+}
