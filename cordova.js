17eed58fcc22f127ecbb4d4808673bf41c08fa90
juansantana-analista <juansantana.analista@gmail.com>
2024-10-04 15:35:29 -0300

    Nome usuario

--- cordova.js
+++ cordova.js
@@ -1006,7 +1006,6 @@ module.exports = {
 
     // example: cordova.commandProxy.add("Accelerometer",{getCurrentAcceleration: function(successCallback, errorCallback, options) {...},...);
     add: function (id, proxyObj) {
-        console.log('adding proxy for ' + id);
         CommandProxyMap[id] = proxyObj;
         return proxyObj;
     },
