fa97159db78fafed7f518b8a111af09570ff9de3
juansantana-analista <juansantana.analista@gmail.com>
2024-10-31 15:23:06 -0300

    update

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,11 +1,7 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-// This is the "Offline page" service worker
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
@@ -21,30 +17,22 @@ self.addEventListener('install', async (event) => {
   );
 });
 
+// Ativar o preload de navegação, mas não usar cache.
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith((async () => {
-      try {
-        // Sempre tente buscar da rede
-        const networkResponse = await fetch(event.request);
-  
-        // Se a resposta for bem-sucedida, retorne-a
-        if (networkResponse.ok) {
-          return networkResponse;
-        } else {
-          // Se houver um erro, você pode optar por retornar uma página offline
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
-        }
-      } catch (error) {
-        // Se a requisição falhar, tente retornar a página offline
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  });
+  event.respondWith((async () => {
+    try {
+      // Sempre tenta buscar a versão mais recente do recurso
+      const networkResp = await fetch(event.request);
+      return networkResp;
+    } catch (error) {
+      // Se falhar ao buscar na rede, retorna a página offline, se disponível
+      const cache = await caches.open(CACHE);
+      const cachedResp = await cache.match(offlineFallbackPage);
+      return cachedResp;
+    }
+  })());
+});
