dc287fded67d2648acc584fcca824cf4af460845
juansantana-analista <juansantana.analista@gmail.com>
2024-10-31 15:01:04 -0300

    update

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -10,8 +10,8 @@ const HOSTNAME_WHITELIST = [
 
 // Função utilitária para corrigir URLs de requisições interceptadas
 const getFixedUrl = (req) => {
-    var now = Date.now();
-    var url = new URL(req.url);
+    const now = Date.now();
+    const url = new URL(req.url);
     url.protocol = self.location.protocol;
 
     if (url.hostname === self.location.hostname) {
@@ -38,40 +38,37 @@ self.addEventListener('activate', event => {
             );
         }).then(() => {
             return self.clients.claim(); // Faz o novo SW controlar todos os clientes
-        }).then(() => {
-            // Atualiza as abas abertas ao ativar o novo Service Worker
-            self.clients.matchAll({ type: 'window' }).then(clients => {
-                clients.forEach(client => client.navigate(client.url));
-            });
         })
     );
 });
 
 // Evento de fetch
 self.addEventListener('fetch', event => {
-    if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
+    if (HOSTNAME_WHITELIST.includes(new URL(event.request.url).hostname)) {
         const cached = caches.match(event.request);
         const fixedUrl = getFixedUrl(event.request);
         const fetched = fetch(fixedUrl, { cache: 'no-store' });
         const fetchedCopy = fetched.then(resp => resp.clone());
 
         event.respondWith(
-            Promise.race([fetched.catch(_ => cached), cached])
+            Promise.race([fetched.catch(() => cached), cached])
                 .then(resp => resp || fetched)
-                .catch(_ => { /* ignore errors */ })
+                .catch(() => { /* ignore errors */ })
         );
 
         event.waitUntil(
-            Promise.all([fetchedCopy, caches.open(CACHE_NAME)]) // Use CACHE_NAME aqui
+            Promise.all([fetchedCopy, caches.open(CACHE_NAME)])
                 .then(([response, cache]) => response.ok && cache.put(event.request, response))
-                .catch(_ => { /* ignore errors */ })
+                .catch(() => { /* ignore errors */ })
         );
     }
 });
 
 // Listener para mensagens de atualização
 self.addEventListener('message', event => {
-    if (event.data === 'skipWaiting') self.skipWaiting();
+    if (event.data === 'skipWaiting') {
+        self.skipWaiting();
+    }
 });
 
 // Evento de push
