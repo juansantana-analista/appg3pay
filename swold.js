4eea3797bd745bb0111c473e152f780261817764
juansantana-analista <juansantana.analista@gmail.com>
2024-11-07 13:18:16 -0300

    Sw

--- swold.js
+++ swold.js
@@ -1,11 +1,7 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-// This is the "Offline page" service worker
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
@@ -21,28 +17,22 @@ self.addEventListener('install', async (event) => {
   );
 });
 
+// Ativar o preload de navegação, mas não usar cache.
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+  event.respondWith((async () => {
+    try {
+      // Sempre tenta buscar a versão mais recente do recurso
+      const networkResp = await fetch(event.request);
+      return networkResp;
+    } catch (error) {
+      // Se falhar ao buscar na rede, retorna a página offline, se disponível
+      const cache = await caches.open(CACHE);
+      const cachedResp = await cache.match(offlineFallbackPage);
+      return cachedResp;
+    }
+  })());
 });
