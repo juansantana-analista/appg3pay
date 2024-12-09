da4286ee2aa902683b829944954d47a230d73d73
juansantana-analista <juansantana.analista@gmail.com>
2024-12-09 20:40:48 -0300

    Alteração

--- js/routes.js
+++ js/routes.js
@@ -27,9 +27,7 @@ var app = new Framework7({
       path: '/index/',
       url: 'index.html?v=1.0.1',
       on: {
-        pageBeforeIn: async function (event, page) {  
-          $("#menuPrincipal").addClass("display-none");
-          $("#menuPrincipal").hide("fast");     
+        pageBeforeIn: async function (event, page) {    
           clearLocalStorage();
           // chama a função que verifica e valida o token
           var userAuthToken = localStorage.getItem('userAuthToken');
@@ -219,8 +217,6 @@ var app = new Framework7({
       on: {
         pageBeforeIn: function (event, page) {
           // fazer algo antes da página ser exibida
-          $("#menuPrincipal").addClass("display-none");
-          $("#menuPrincipal").hide("fast");
         },
         pageAfterIn: function (event, page) {
           // fazer algo depois da página ser exibida
