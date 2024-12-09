669a075b0185fb8c6be3ea6f25a585ad98b76e80
juansantana-analista <juansantana.analista@gmail.com>
2024-12-09 18:50:27 -0300

    Alteração

--- js/routes.js
+++ js/routes.js
@@ -761,7 +761,7 @@ var app = new Framework7({
     },
     {
       path: '/produtos/',
-      url: 'produtos.html',
+      url: 'produtos.html?v=1.0.1',
       animate: false,
       on: {
         pageBeforeIn: async function (event, page) {
