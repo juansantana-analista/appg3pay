c3dc21205f1edc5c0917fbdaa779ba93d48c94c5
juansantana-analista <juansantana.analista@gmail.com>
2025-02-05 10:24:20 -0300

    alteração do login form

--- js/funcoes.js
+++ js/funcoes.js
@@ -1693,12 +1693,12 @@ function listarEnderecos() {
             }).get().join("<br>");
   
             $("#selectedAddress").html(`
+              <div class="flex items-start space-x-3">
                 <div class="flex items-center space-x-2">
                   <h3 class="font-medium">${enderecoSelecionado.nome_endereco || "Residencial"}</h3>
                   ${enderecoSelecionado.principal == "S" ? 
                     `<span class="px-2 py-0.5 text-white text-xs rounded-full" style="background-color: #ff7b39">Principal</span>` : ""}
                 </div>
-              <div class="flex items-start space-x-3">
                 <svg class="w-5 h-5 text-gray-600 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
