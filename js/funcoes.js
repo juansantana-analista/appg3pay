e1951782ce70427fbb3aa3d8ba00d8eef2660a69
juansantana-analista <juansantana.analista@gmail.com>
2025-02-05 13:25:46 -0300

    alteração do login form

--- js/funcoes.js
+++ js/funcoes.js
@@ -1530,7 +1530,7 @@ function listarEnderecos() {
                   </p>
                 </div>
                 <div class="flex items-center space-x-2">
-                  <button class="text-gray-400 hover:text-gray-600 edit-address" data-id="${endereco.id}">
+                  <button class="text-gray-400 hover:text-gray-600 edit-address">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
