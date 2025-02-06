73cc9a2995ce1c4ed3daca0fc7a0af6cb83418df
juansantana-analista <juansantana.analista@gmail.com>
2025-02-06 09:56:21 -0300

    Alteração função endereço

--- js/routes.js
+++ js/routes.js
@@ -1137,6 +1137,19 @@ var app = new Framework7({
             });
           });
 
+          $("#btnDesconto").on('click', function () {
+            var cupomDesconto = $("#cupomDesconto").val();
+            if(cupomDesconto) {
+              console.log(cupomDesconto);
+            } else {
+              app.dialog.alert("Digite um cupom de Desconto", "Desconto");
+            }
+            app.dialog.confirm('Tem certeza que quer esvaziar o carrinho?', '<strong>ESVAZIAR</strong>', function () {
+              //Chamar a funçao que limpa o carrinho
+              limparCarrinho();
+            });
+          });
+
           //INICIO API CEP PARA ENDEREÇO DE NOVO CLIENTE
           $('#cepCliente').mask('00000-000');
           const cepInput = document.getElementById('cepCliente');
