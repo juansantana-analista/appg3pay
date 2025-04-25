91072890edf32a9afd17deaf5c6116cc828c36f4
juansantana-analista <juansantana.analista@gmail.com>
2025-04-25 14:19:49 -0300

    Alteração ola usuario

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 //DADOS BACKEND SERVER
 const apiServerUrl = "https://vitatop.tecskill.com.br/rest.php";
-const versionApp = "1.0.9";
+const versionApp = "1.1.0";
 
 //INICIALIZAÇÃO DO F7 QUANDO DISPOSITIVO ESTÁ PRONTO
 document.addEventListener('deviceready', onDeviceReady, false);
