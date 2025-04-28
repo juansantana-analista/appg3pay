e94cf53c2c56e0bb34a426ffc81d9b0c0dff8fba
juansantana-analista <juansantana.analista@gmail.com>
2025-04-28 17:47:41 -0300

    Alteração categorias

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 //DADOS BACKEND SERVER
 const apiServerUrl = "https://vitatop.tecskill.com.br/rest.php";
-const versionApp = "1.1.2";
+const versionApp = "1.1.3";
 
 //INICIALIZAÇÃO DO F7 QUANDO DISPOSITIVO ESTÁ PRONTO
 document.addEventListener('deviceready', onDeviceReady, false);
