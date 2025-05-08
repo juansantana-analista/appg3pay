83382b62abba963dec6cd207bd429e169805a098
juansantana-analista <juansantana.analista@gmail.com>
2025-05-08 17:42:29 -0300

    Campanhas

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 //DADOS BACKEND SERVER
 const apiServerUrl = "https://vitatop.tecskill.com.br/rest.php";
-const versionApp = "1.2.1";
+const versionApp = "1.2.2";
 var userAuthToken = "";
 
 //INICIALIZAÇÃO DO F7 QUANDO DISPOSITIVO ESTÁ PRONTO
