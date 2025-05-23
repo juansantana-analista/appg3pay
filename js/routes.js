9b9dff5d70f5bcd47bb29e7c8a8ecded7469965f
juansantana-analista <juansantana.analista@gmail.com>
2025-05-23 17:09:06 -0300

    Alteração tela perfil

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 //DADOS BACKEND SERVER
 const apiServerUrl = "https://vitatop.tecskill.com.br/rest.php";
-const versionApp = "1.9.4";
+const versionApp = "1.9.5";
 var userAuthToken = "";
 
 //INICIALIZAÇÃO DO F7 QUANDO DISPOSITIVO ESTÁ PRONTO
