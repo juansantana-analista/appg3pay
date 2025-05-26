9f7148cff412b70c1a00abb4bb580bd835df1fde
juansantana-analista <juansantana.analista@gmail.com>
2025-05-26 08:49:40 -0300

    Alteração tela perfil

--- js/routes.js
+++ js/routes.js
@@ -61,6 +61,8 @@ var app = new Framework7({
               $('.menu-tab-link').removeClass('menu-active');
               app.views.main.router.navigate("/perfil/"); 
           });
+  inicializarMenuLateral();
+  atualizarTabbarComMenu();
 
       listarPerfil("index");
   
